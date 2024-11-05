432d1ceee460c88b3c028a8c84442f8298f814e5
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-11-05 13:59:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class hello:
     def GET(self, name):
         if not name:
             name = 'World'
-        return 'Hello welcome, ' + name + '!'
+        return 'Hello Welcome, ' + name + '!'
 
 if __name__ == "__main__":
     app.run()
